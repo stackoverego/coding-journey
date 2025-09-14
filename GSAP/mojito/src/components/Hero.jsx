280f28359e5e48221d049bf96cab5df73801e940
parth <parthpatil1610@gmail.com>
2025-09-14 10:53:34 +0530

    hero video added

--- GSAP/mojito/src/components/Hero.jsx
+++ GSAP/mojito/src/components/Hero.jsx
@@ -2,8 +2,12 @@ import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import { SplitText } from "gsap/all";
 import React from "react";
+import { useRef } from "react";
+import { useMediaQuery } from "react-responsive";
 
 const Hero = () => {
+  const videoRef=useRef(null)
+  const isMobile=useMediaQuery({maxWidth:767})
     useGSAP(()=>{
         const heroSplit =new SplitText(".title",{type:"chars ,words"});
         const paragraphSplit =new SplitText(".subtitle",{type:"lines"});
@@ -35,6 +39,26 @@ const Hero = () => {
         })
         .to('.right-leaf',{y:200},0)
         .to('.left-leaf',{y: -200},0)
+
+        const startvalue=isMobile ? "top 50%":"center 60%";
+        const endvalue=isMobile ? "120% top ":"bottom top";
+        
+       const tl=gsap.timeline({
+          scrollTrigger:{
+            trigger:"video",
+            start:startvalue,
+            end:endvalue,
+            scrub:true,
+            pin:true,
+          }
+        })
+
+          videoRef.current.onloadedmetadata=()=>{
+            tl.to(videoRef.current,{
+              currentTime:videoRef.current.duration
+
+            })
+          }
     },[])
   return (
     <>
@@ -70,6 +94,13 @@ const Hero = () => {
           </div>
         </div>
       </section>
+      <div className=" video absolute inset-0"> 
+            <video 
+            src="/videos/output.mp4" 
+            muted playsInline preload="auto"
+            ref={videoRef}></video>
+            
+      </div>
     </>
   );
 };
